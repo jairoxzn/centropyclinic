291228498660d61b9c58ddbbe7a496199115c114
jairoxzn <jairolajo4@gmail.com>
2026-07-31 22:31:10 -0500

    fix: remove incorrect prisma adapter

--- config/database.js
+++ config/database.js
@@ -1,12 +1,7 @@
 require('dotenv').config();
 const { PrismaClient } = require('@prisma/client');
-const { PrismaPg } = require('@prisma/adapter-pg');
-
-const connectionString = process.env.DATABASE_URL;
-const adapter = new PrismaPg({ connectionString });
 
 const prisma = new PrismaClient({
-  adapter,
   log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
 });
 
